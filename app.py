c11a312f5fe331ca0f6f058912f80c31bf5acdda
vdeb <vdebebe924@gmail.com>
2020-11-19 16:00:58 -0500

    updates

--- app.py
+++ app.py
@@ -10,70 +10,63 @@ from predict import render_10
 import csv
 import sqlite3
 
-app = Flask(__name__)
-Bootstrap(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-DB = SQLAlchemy(app)
-app.config['SECRET_KEY'] = '<SECRET_KEY>'
-csrf = CSRFProtect(app)
-#conn = connect("sqlite:///spotify.db")
-#curs = conn.cursor()
-#with open('herokuspotify.csv', 'r') as Spotify_df:
-#df = pandas.read_csv('herokuspotify.csv')
-#df.to_sql(dataset, conn, if_exists='append', index=False)
+
+def create_app():
+    app = Flask(__name__)
+    Bootstrap(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    DB = SQLAlchemy(app)
+    app.config['SECRET_KEY'] = '<SECRET_KEY>'
+    csrf = CSRFProtect(app)
+
+    @app.route('/', methods=['GET'])
+    def dropdown():
+        
+        #return 'This is where the main app page will appear'
+        return render_template('base.html')
+
+    #data = pd.read_csv('/herokuspotify.csv')
 
 
-#return app
+    # @app.route('/predict', methods=['POST'])
+    # def predict():
+        
+        # prediction = render_10(chosen_song)
 
-@app.route('/', methods=['GET'])
-def dropdown():
-    songs = ['one', 'two', 'three', 'four']
-      
-    #return 'This is where the main app page will appear'
-    return render_template('base.html', songs=songs)
+    @app.route('/predict', methods=['GET', 'POST'])
+    def home():
 
-#data = pd.read_csv('/herokuspotify.csv')
-
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-    
-    # prediction = render_10(chosen_song)
-
-@app.route('/predict', methods=['GET', 'POST'])
-def home():
-
-    return render_10(request.values['song'])
+        return render_10(request.values['song'])
 
 
 
 
-def results():
-    return render_template('BS.html', title='Predict and Listen')
+    def results():
+        return render_template('BS.html', title='Predict and Listen')
 
-@app.route('/about')
-def about():
-    return render_template('about.html', title='Spotify Pick Me - About')
-
-
-@app.route('/register')
-def register():
-    form = RegistrationForm()
-    return render_template('register.html', title='Register', form=form)
+    @app.route('/about')
+    def about():
+        return render_template('about.html', title='Spotify Pick Me - About')
 
 
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Login', form=form)
-
-@app.route('/process')
-def process():
-    return render_template('process.html', title='Our Process in Creating Predictify')
+    @app.route('/register')
+    def register():
+        form = RegistrationForm()
+        return render_template('register.html', title='Register', form=form)
 
 
+    @app.route('/login')
+    def login():
+        form = LoginForm()
+        return render_template('login.html', title='Login', form=form)
+
+    @app.route('/process')
+    def process():
+        return render_template('process.html', title='Our Process in Creating Predictify')
+
+    return app
     
     
 if __name__ == "__main__":
-    app.run(debug=True)   
+    app.run_server(debug=True)   
